a7e95eb422ef045f6036255e2e7700669ed2bd36
Joseph Kinahan <kinahanj25@gmail.com>
2022-08-30 00:50:10 -0400

    bones of BE, spring booted, player entity

---
+++ src/main/java/com/majortomdev/SymphBE/SymphBeApplication.java
@@ -0,0 +1,13 @@
+package com.majortomdev.SymphBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymphBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymphBeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/majortomdev/SymphBE/models/Player.java
@@ -0,0 +1,53 @@
+package com.majortomdev.SymphBE.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "players")
+public class Player {
+    private long id;
+    private String playerName;
+    private Position position;
+    private double value;
+
+    public Player() {
+
+    }
+
+    enum Position {
+        GOALKEEPER, DEFENDER, MIDFIELDER, ATTACKER
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
